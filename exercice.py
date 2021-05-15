df229542306289e2b1bc20f64df4522c9fce25a1
bASILgIL <70984519+bASILgIL@users.noreply.github.com>
2021-05-15 03:02:36 -0400

    update

--- exercice.py
+++ exercice.py
@@ -3,32 +3,46 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	a = ("La puissance dissipée par la résistance est de", str(round(voltage**2/resistance, 2)),"W.")
+	separateur = " "
+	return separateur.join(a)
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
 	v1[0] # Pour accéder au X
 	v1[1] # Pour accéder au Y
-	pass
+	return (v1[0]*v2[0] + v1[1]*v2[1]) == 0
+
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	new = []
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v >= 0:
+			new.append(v)
+	return sum(new)/len(new)
+		 # La variable v contient une valeur de la liste.
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	separateur = ", "
 	while value != 0:
 		if value >= 20:
-			pass
+			twenties = value//20
+			value = value - twenties * 20
 		elif value >= 10:
-			pass
+			tens = value//10
+			value = value - tens * 10
 		elif value >= 5:
-			pass
-		elif value >= 1:
-			pass
+			fives = value//5
+			value = value - fives * 5
+		elif value >= 2:
+			twos = value//2
+			value = value - twos * 2
+			ones = value
 
-	return (twenties, tens, fives, twos, ones);
+
+	return twenties, tens, fives, twos, ones
 
 if __name__ == "__main__":
 	print(dissipated_power(69, 420))
